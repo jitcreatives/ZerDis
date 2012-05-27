2ccb31068cd5550d9a84cc3a1acc1e97032dcb14
Jörg Bachmann <explicit@explicits.de>
2012-05-27 18:11:20 +0200

    New Literal: system call
    * Good for using external scripts

---
+++ logic/LiteralSystemCall.py
@@ -0,0 +1,19 @@
+
+import subprocess
+from String import Template
+
+from KnfLiteral import KnfLiteral
+
+class LiteralSystemCall( KnfLiteral ):
+
+        def __init__( self, negated, pattern ):
+                KnfLiteral.__init__( self, negated )
+                self.pattern = pattern
+
+        def check( self, context ):
+                syscall = Template( self.pattern ).substitude( context.context_map )
+
+                if 0 == subprocess.call( syscall, shell = True ):
+                        return True
+                else:
+                        return False
