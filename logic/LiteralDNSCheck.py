1a64603a87ca1bd99ddb61dcb5803edb3a19b2a2
jit <jit-list@jitmail.de>
2012-05-28 19:38:26 +0200

    Use hostbyaddr not hostbyname in DNSCheck

--- logic/LiteralDNSCheck.py
+++ logic/LiteralDNSCheck.py
@@ -9,8 +9,15 @@ class LiteralDNSCheck( KnfLiteral ):
                 KnfLiteral.__init__( self, negated )
 
         def check( self, context ):
-                ip = socket.gethostbyname( context.context_map[ 'domain' ] )
-                if ip == context.context_map[ 'ip' ]:
-                        return True
-                else:
-                        return False
+                #ip = socket.gethostbyname( context.context_map[ 'domain' ] )
+		hostname = socket.gethostbyaddr( context.context_map[ 'ip' ] )[0]
+
+		#print hostname
+		#print ip
+
+		#for hostname in hostnames:
+		if hostname == context.context_map[ 'domain' ]:
+		#if ip == context.context_map[ 'ip' ]:
+			return True
+
+		return False
